92fa6623b499726f7c327aeadee15d683acef981
15227241216 <1469442737@qq.com>
2022-01-08 22:52:20 +0800

    更新第二题

---
+++ algorithm/searchInsert/demo1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  const arr = [...new Set([...nums, target])]
+  arr.sort((a, b) => a - b)
+  return arr.indexOf(target)
+}
+
+console.log(searchInsert([1, 3, 5, 6], 5))
+console.log(searchInsert([1, 3, 5, 6], 2))
+console.log(searchInsert([1, 3, 5, 6], 7))
+console.log(searchInsert([1, 3, 5, 6], 0))
+console.log(searchInsert([1], 0))
