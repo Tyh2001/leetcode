90a81c587f860ad2a18a509238177b9a3aca32f0
15227241216 <1469442737@qq.com>
2022-01-08 22:12:07 +0800

    更新

--- algorithm/search/demo1.js
+++ algorithm/search/demo1.js
@@ -4,7 +4,8 @@
  * @return {number}
  */
 var search = function (nums, target) {
-
+  return nums.indexOf(target)
 }
 
-console.log(search([-1, 0, 3, 5, 9, 12], 9))
+// console.log(search([-1, 0, 3, 5, 9, 12], 9))
+console.log(search([-1, 0, 3, 5, 9, 12], 2))
