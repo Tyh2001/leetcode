9955491385d213f698e01fc2be7dbfb403ad1d54
15227241216 <1469442737@qq.com>
2022-01-09 11:12:48 +0800

    算法太难了啊

--- algorithm/search/demo1.js
+++ algorithm/search/demo1.js
@@ -7,5 +7,5 @@ var search = function (nums, target) {
   return nums.indexOf(target)
 }
 
-// console.log(search([-1, 0, 3, 5, 9, 12], 9))
+console.log(search([-1, 0, 3, 5, 9, 12], 9))
 console.log(search([-1, 0, 3, 5, 9, 12], 2))
