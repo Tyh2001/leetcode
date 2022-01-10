7fc8f7b6206db361d58d78e39398a201b9864815
Tyh2001 <1469442737@qq.com>
2022-01-10 17:52:27 +0800

    难啊

--- algorithm/1.search/demo1.js
+++ algorithm/1.search/demo1.js
@@ -8,5 +8,8 @@ var search = function (nums, target) {
   return nums.indexOf(target)
 }
 
-console.log(search([-1, 0, 3, 5, 9, 12], 9))
-console.log(search([-1, 0, 3, 5, 9, 12], 2))
+console.log(search([1, 3, 5, 6], 5))
+console.log(search([1, 3, 5, 6], 2))
+console.log(search([1, 3, 5, 6], 7))
+console.log(search([1, 3, 5, 6], 0))
+console.log(search([1], 0))
