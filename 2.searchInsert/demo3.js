b4c90545e977856231c6835cf1f14f0044dbb86d
Tyh2001 <1469442737@qq.com>
2022-01-19 18:01:08 +0800

    更新算法

---
+++ 2.searchInsert/demo3.js
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode-cn.com/problems/search-insert-position/
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var searchInsert = function (nums, target) {
+  let firstIndex = 0,
+    lastIndex = nums.length - 1
+
+  while (firstIndex <= lastIndex) {
+    const middleIndex = Math.floor((firstIndex + lastIndex) / 2)
+    if (nums[middleIndex] === target) {
+      return middleIndex
+    } else if (nums[middleIndex] > target) {
+      lastIndex = middleIndex - 1
+    } else if (nums[middleIndex] < target) {
+      firstIndex = middleIndex + 1
+    } else {
+      return middleIndex
+    }
+  }
+  return lastIndex + 1
+}
+
+console.log(searchInsert([1, 3, 5, 6], 5)) // 2
+console.log(searchInsert([1, 3, 5, 6], 2)) // 1
+console.log(searchInsert([1, 3, 5, 6], 7)) // 4
+console.log(searchInsert([1, 3, 5, 6], 0)) // 0
+console.log(searchInsert([1], 0)) // 0
