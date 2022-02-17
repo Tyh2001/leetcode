043af727d88ee851596c2aef9d68e7bd249819e3
Tyh2001 <1469442737@qq.com>
2022-02-17 10:13:25 +0800

    更新两数之和

---
+++ 3.twoSum/demo1.js
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode-cn.com/problems/two-sum/
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+
+}
+
+console.log(twoSum([2, 7, 11, 15]), 9) // [0, 1]
+console.log(twoSum([3, 2, 4]), 6) // [1, 2]
+console.log(twoSum([3, 3]), 6) // [0, 1]
