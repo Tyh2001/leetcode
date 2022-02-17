0f35616f81b90f9bb709cd61a5df2968d54a69b5
Tyh2001 <1469442737@qq.com>
2022-02-17 12:20:59 +0800

    准备更新罗马数字

---
+++ 5.romanToInt/demo1.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode-cn.com/problems/roman-to-integer/
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+
+}
+
+console.log(romanToInt(III)) // 3
+console.log(romanToInt(IV)) // 4
+console.log(romanToInt(IX)) // 9
+console.log(romanToInt(LVIII)) // 58 因为 : L = 50, V= 5, III = 3
+console.log(romanToInt(MCMXCIV)) // 58 因为 : M = 1000, CM = 900, XC = 90, IV = 4
