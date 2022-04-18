6cda2fef75393d2f6f7d386f9136a16a667c3b76
tyh2001 <1469442737@qq.com>
2022-04-18 15:26:00 +0800

    更新14题

---
+++ 14.ts
@@ -0,0 +1,8 @@
+// https://leetcode-cn.com/problems/longest-common-prefix/
+function longestCommonPrefix(strs: string[]): string {
+
+  return ''
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"])) // fl
+console.log(longestCommonPrefix(["dog", "racecar", "car"])) // ''
